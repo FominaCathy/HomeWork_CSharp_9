433374a9e9c0ab7d4f5dbfeea02c19a4b2213227
FominaCathy <99419802+FominaCathy@users.noreply.github.com>
2022-04-13 13:26:59 +0400

    добавила доп задачу 3 (возведение в степень)

---
+++ HW9_ADD_Task_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADD_Task_3
+{
+    //Задача 3. Даны натуральные числа a и b.
+    //Рекурсивно описать функцию возведения числа a в степень b,
+    //используя только операцию инкрементирования (“++”).
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseA = 5; //основание,
+            int degreeB = 3; //степень
+            int result = baseA;
+            ;
+            int step = 1;
+
+            Console.WriteLine( $"{baseA} в степени {degreeB} = {DegreeConcaten(baseA, degreeB)}");
+            Console.ReadLine();
+            
+            int DegreeConcaten(int a, int b, int count = 1)
+            {
+                if (count == baseA)
+                {
+                    step++;
+                    DegreeConcaten(result, b);
+                }
+                if (step == b)
+                    return result;
+        
+                for (int i = 0; i < a; i++) result++;
+                count++;
+                return DegreeConcaten(a, b, count);
+
+            }
+        }
+    }
+}
