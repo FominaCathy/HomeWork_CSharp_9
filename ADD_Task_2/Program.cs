0260af358e09273bbe83d4d0e94b0b1b171f5674
FominaCathy <99419802+FominaCathy@users.noreply.github.com>
2022-04-13 12:32:29 +0400

    добавила доп задачу 2. сделал по лекции

---
+++ ADD_Task_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADD_Task_2
+{
+    //Задача 2.
+    //Известно, что пароль длиной 3 символа
+    //состоит из латинских букв строчного регистра и цифр от 0 до 9.
+    //Напишите рекурсивный метод, который перебирает все комбинации паролей
+
+    //сделала по лекции
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string alfavit = "qwertyuiopasdfghjklzxcvbnm0123456789";
+           
+            FillRassword(alfavit, new char[3]);
+
+            Console.ReadLine();
+
+            void FillRassword(string str, char[] pass, int lengthPass = 0)
+            {
+                if (lengthPass == pass.Length)
+                {
+                    Console.WriteLine(new String(pass));
+                    return;
+                }
+
+                for (int i = 0; i < str.Length; i++)
+                {
+                    pass[lengthPass] = str[i];
+                    FillRassword(str, pass, lengthPass + 1);
+                }
+            }
+
+        }
+    }
+}
